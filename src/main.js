b82aa34b66c29a0a061b7897ef04a9a61dd7968e
MD <mees.dekker@ing.com>
2024-10-15 19:49:37 +0200

    deleted logo and hopefully fixed connection to camera kit

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import {
 	Transform2D,
 } from '@snap/camera-kit'
 
-(async function()){
-	var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI4OTEwNTE0LCJzdWIiOiIzZjcyYTFhZi04OWM5LTQ0ZDUtOTk1OS1mM2NlN2JhY2I4NWF-U1RBR0lOR34yZTE4ZjI1OS00ZDJkLTQ4Y2MtOWUwOS1iNzIwNmI1ZDUzOTMifQ.X6FJhwhVlEpEGK0Nj4W51OUYwPvdSy0_UYozvgZMfSo'})
+(async function(){
+	var cameraKit = await bootstrapCameraKit({ 
+		apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI4OTEwNTE0LCJzdWIiOiIzZjcyYTFhZi04OWM5LTQ0ZDUtOTk1OS1mM2NlN2JhY2I4NWF-U1RBR0lOR34yZTE4ZjI1OS00ZDJkLTQ4Y2MtOWUwOS1iNzIwNmI1ZDUzOTMifQ.X6FJhwhVlEpEGK0Nj4W51OUYwPvdSy0_UYozvgZMfSo'
+	  });
 
 	const session = await cameraKit.createSession()
-	document.getelementById('canvas').replaceWith(session.output.live)
+	document.getElementById('canvas').replaceWith(session.output.live)
 
-	const { lenses } = await cameraKit.lensRepository.loadLensGroups(['c7c1a51a-f7bd-4f21-8dd1-6df59e4c40e0'])
+	const { lenses } = await cameraKit.lensRepository.loadLensGroups(['5efcc387-2d93-4538-af28-abbf86265c97'])
 
 	session.applyLens(lenses[0])
 
@@ -22,7 +24,7 @@ import {
 		cameraType: 'back'
 	})
 
-	await session.setSrouce(source)
+	await session.setSource(source)
 
 	session.source.setRenderSize(window.innerWidth, window.innerHeight)
 
